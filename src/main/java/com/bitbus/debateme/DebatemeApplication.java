a70590f766a0ae60aaf9dc522aa7824450bc5f79
Joe Prochnow <joe.prochnow@neovera.com>
2019-01-30 16:46:39 -0500

    Stub initial backend

---
+++ src/main/java/com/bitbus/debateme/DebatemeApplication.java
@@ -0,0 +1,14 @@
+package com.bitbus.debateme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebatemeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DebatemeApplication.class, args);
+	}
+
+}
+
